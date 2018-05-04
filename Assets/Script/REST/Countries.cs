9fa8f06ee00ccc5e38c0585aa6da82cd87e3d6ab
danielnguyen1401 <nguyen.leenguyen@gmail.com>
2018-05-04 16:04:03 +0700

    REST api: can read json from web, and get data, order data

---
+++ Assets/Script/REST/Countries.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class Countries : MonoBehaviour
+{
+    Dropdown ddlCountries;
+    [SerializeField] Text countryValueText;
+
+    void Start()
+    {
+        ddlCountries = GetComponent<Dropdown>();
+        ddlCountries.ClearOptions();
+        StartCoroutine(GetCountries());
+    }
+
+
+    IEnumerator GetCountries()
+    {
+        string getCountriesUrl = "https://restcountries.eu/rest/v2/all";
+        UnityWebRequest www = UnityWebRequest.Get(getCountriesUrl);
+
+        yield return www.SendWebRequest();
+
+        if (www.isNetworkError || www.isHttpError)
+        {
+            Debug.Log(www.error);
+        }
+        else
+        {
+            if (www.isDone)
+            {
+                string jsonResult = System.Text.Encoding.UTF8.GetString(www.downloadHandler.data);
+//                Debug.Log(jsonResult);
+                RootObject[] countries = JsonHelper.GetJsonArray<RootObject>(jsonResult);
+
+                ddlCountries.options.AddRange(countries.OrderBy(p => p.name)
+                    .Select(x => new Dropdown.OptionData() {text = x.nativeName}).ToList());
+
+                ddlCountries.value = 0;
+            }
+        }
+    }
+
+    public void OnValueChange()
+    {
+        countryValueText.text = ddlCountries.options[ddlCountries.value].text;
+    }
+
+    void Update()
+    {
+    }
+}
